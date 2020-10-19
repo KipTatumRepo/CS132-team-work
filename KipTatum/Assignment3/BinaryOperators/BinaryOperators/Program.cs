379e5ecdd01ab45c6aca3b981448585322eee9bf
Kip Tatum <tatumkip@cityuniversity.edu>
2020-10-19 11:28:14 -0700

    adding PE 3 need to add header on next commit

---
+++ KipTatum/Assignment3/BinaryOperators/BinaryOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BinaryOperators
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//declare and assign the variables
+			int a = 100;
+			int b = 20;
+
+			//I wasn't sure if we should reassign the value of 'a' back to
+			//its original value of 100 each time.  In this case 'a' just keeps is
+			//always equal to the result of the previous operation and never 
+			//reassigned back to 100, unless that is the result of the operation.
+			//I also thought it made the code a bit more concise to include the operation
+			//in the WriteLine method
+			Console.WriteLine("The value of a is now:{0}", a += b);
+			Console.WriteLine("The value of a is now:{0}", a -= b);
+			Console.WriteLine("The value of a is now:{0}", a *= b);
+			Console.WriteLine("The value of a is now:{0}", a /= b);
+			Console.WriteLine("The value of a is now:{0}", a %= b);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ KipTatum/Assignment3/Random/Random/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Randoms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//set the constants for minimum and maximum values
+			const int MIN = 1;
+			const int MAX = 100;
+
+			//create one instance of the random object
+			Random randomNumber = new Random();
+			
+			//declare and assign the variables to be random nubmers between 1 and 100 
+			int a = randomNumber.Next(MIN, MAX);
+			int b = randomNumber.Next(MIN, MAX);
+
+			//display the created random number
+			Console.WriteLine("The value of the first random number is:{0}", a);
+			Console.WriteLine("The value of the second random number is:{0}", b);
+
+
+			//I wasn't sure if we should reassign the value of 'a' back to
+			//its original value of 100 each time.  In this case 'a' just keeps is
+			//always equal to the result of the previous operation and never 
+			//reassigned back to 100, unless that is the result of the operation.
+			//I also thought it made the code a bit more concise to include the operation
+			//in the WriteLine method
+			Console.WriteLine("The value of a is now:{0}", a += b);
+			Console.WriteLine("The value of a is now:{0}", a -= b);
+			Console.WriteLine("The value of a is now:{0}", a *= b);
+			Console.WriteLine("The value of a is now:{0}", a /= b);
+			Console.WriteLine("The value of a is now:{0}", a %= b);
+			Console.ReadKey();
+		}
+	}
+}
