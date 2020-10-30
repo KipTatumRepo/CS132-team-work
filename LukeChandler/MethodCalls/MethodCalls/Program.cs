380390a9d5ec8fe10b1f56060e7e27c7df11661e
lukehc <lukehc@amazon.com>
2020-10-30 15:10:43 -0700

    PE04

---
+++ LukeChandler/MethodCalls/MethodCalls/Program.cs
@@ -0,0 +1,43 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS 132
+**Project: PE04 Exercise 1
+**Date: 2020-10-30
+**Description: Calling methods to display a student grade average and name"
+*********/
+
+using System;
+
+namespace MethodCalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring the Constants
+            const float MATH_GRADE = 80.0f;
+            const float BIOLOGY_GRADE = 80.0f;
+            const float PROGRAMMING_GRADE = 92.0f;
+            const string STUDENT_NAME = "G. Treele";
+
+            //Calling the CalculateAverage method
+            float average = CalculateAverage(MATH_GRADE, BIOLOGY_GRADE, PROGRAMMING_GRADE);
+
+            //Displaying the Result
+            ShowAverage(STUDENT_NAME, average);
+            Console.ReadKey();
+        }
+
+        //Creating the CalculateAverage method to average the Math, Biology, and Programming grades
+        static float CalculateAverage(float mathGrade, float biologyGrade, float programmingGrade)
+        {
+            return (mathGrade + biologyGrade + programmingGrade) / 3;
+        }
+
+        //Creating the ShowAverage Method to display the sudents name and average in "G. Treele average: 84" format
+        static void ShowAverage(string studentName, float gradeAverage)
+        {
+            Console.WriteLine($"{studentName} average: {gradeAverage}");
+        }
+    }
+}
---
+++ LukeChandler/ScopeLevels/ScopeLevels/Program.cs
@@ -0,0 +1,44 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS 132
+**Project: PE04 Exercise 3
+**Date: 2020-10-30
+**Description: Playing around with Scope and its effect on variables."
+*********/
+
+using System;
+
+namespace ScopeLevels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring 2 Variables at Class level
+            int classAndMethodLevel = 24;
+            int classAndBlockLevel = 48;
+
+            //What they Output
+            Console.WriteLine("Class Level: " + classAndMethodLevel);
+            Console.WriteLine("Class Level: " + classAndBlockLevel);
+            ShowLocalizedValues();
+            Console.ReadKey();
+        }
+
+        //Making the method
+        static void ShowLocalizedValues()
+        {
+            const bool ENTER_CONDITION = true;
+            int classAndMethodLevel = 72;
+            Console.WriteLine("Method Level: " + classAndMethodLevel);
+            if (ENTER_CONDITION)
+            {
+                int classAndBlockLevel = 12;
+                Console.WriteLine("Block Level: " + classAndBlockLevel);
+            }
+
+        }
+
+    }
+
+}
