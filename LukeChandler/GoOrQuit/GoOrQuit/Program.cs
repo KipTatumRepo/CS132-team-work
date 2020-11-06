20282f1962ca2af05fa10e1243d542801598c324
lukehc <lukehc@amazon.com>
2020-11-06 12:04:26 -0800

    PE05_Luke

---
+++ LukeChandler/GoOrQuit/GoOrQuit/Program.cs
@@ -0,0 +1,43 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS 132
+**Project: PE05 Exercise 4
+**Date: 2020-11-06
+**Description: While loop calling a method and asking for input whether to conitnue or quit the loop
+*********/
+
+using System;
+
+namespace GoOrQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool inputReader = true;
+
+            while (inputReader)
+            {
+                IsItDone();
+                string doneYet = Console.ReadLine(); //asking for user input
+                if (doneYet == "2") //Choose option that user is done and will exit from loop
+                    break;
+                else // keep going in the loop
+                {
+                    Console.WriteLine("You still have a ways to go.");
+                    Console.WriteLine("");
+                }
+            }//Loop is finished
+            Console.WriteLine("Go Home, you are DONE");
+            Console.ReadLine();
+        }
+
+        public static void IsItDone()
+        {
+            //Find if the person is done
+            Console.WriteLine("Complete the Task");
+            Console.WriteLine("1. Keep Going. Not Done Yet");
+            Console.WriteLine("2. You're Done. You can Stop");
+        }
+    }
+}
---
+++ LukeChandler/WarnLevel/WarnLevel/Program.cs
@@ -0,0 +1,70 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS 132
+**Project: PE05 Exercise 1 and 2
+**Date: 2020-11-06
+**Description: Playing with if...else statements and AND/OR operators
+*********/
+
+
+using System;
+
+namespace WarnLevel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Running the different Scenerios
+
+            //Expected: Warning Level is Low! 
+            WarningLevel(1, false);
+            //Expected: False Alarm
+            WarningLevel(1, true);
+            //Expected: Warning Level is Medium!
+            WarningLevel(2, false);
+            //Expected: False Alarm
+            WarningLevel(2, true);
+            //Expected: Warning Level is High!
+            WarningLevel(3, false);
+            //Expected: False Alarm
+            WarningLevel(3, true);
+            //Expected: No Warning!
+            WarningLevel(0, false);
+        }
+
+        //Defining method to take an int and a boolean
+        public static void WarningLevel(int warning, bool falseAlarm)
+        {
+            //if else statement with AND and OR operators 
+            if (warning == 1 && falseAlarm == false)
+            {
+                Console.WriteLine("Warning Level is Low!");
+            }
+            else if (warning == 2 && falseAlarm == false)
+            {
+                Console.WriteLine("Warning Level is Medium!");
+            }
+            else if (warning == 3 && falseAlarm == false)
+            {
+                Console.WriteLine("Warning Level is High!");
+            }
+            else if (warning == 1 || falseAlarm == true)
+            {
+                Console.WriteLine("False Alarm");
+            }
+            else if (warning == 2 || falseAlarm == true)
+            {
+                Console.WriteLine("False Alarm");
+            }
+            else if (warning == 3 || falseAlarm == true)
+            {
+                Console.WriteLine("False Alarm");
+            }
+            else
+            {
+                Console.WriteLine("No Warning!");
+            }
+        }
+    }
+}
