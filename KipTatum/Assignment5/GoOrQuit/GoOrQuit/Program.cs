d5355af404ded83b2d2de80d81942149abd9a3f4
Kip Tatum <tatumkip@cityuniversity.edu>
2020-11-08 14:00:21 -0800

    Assignment 5 Code

---
+++ KipTatum/Assignment5/GoOrQuit/GoOrQuit/Program.cs
@@ -0,0 +1,45 @@
+﻿/********
+**
+**Name: Tatum, Kip
+**Class: CS 132
+**Project: 05 
+**Date: 2020-02-11
+**Description: A program that will continue to work until
+** told to exit by the user
+*********/
+using System;
+
+namespace GoOrQuit
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool run = true; //we will update this when user has chosen to exit
+			while(run) 
+			{
+				CreateMenu();
+				string input = Console.ReadLine();
+				Console.WriteLine();
+				
+				//user has chosen to exit we need to update the boolen and print 
+				//good-bye message
+				if (input == "2")
+				{
+					run = false;
+					Console.WriteLine("Bye Bye.  Press any key to exit.");
+				}
+			}
+			Console.ReadKey();
+		}
+		//This method creates a simple menu with 2 options for a user to select from
+		static void CreateMenu()
+		{
+			Console.WriteLine("Program is Running");
+			Console.WriteLine("Select an option");
+			Console.WriteLine("1. Keep running the loop");
+			Console.WriteLine("2. Stop the loop");
+			
+		}
+	}
+}
