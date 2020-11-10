6b96daae9b1060538b225e224936c961a710f981
Kip Tatum <tatumkip@cityuniversity.edu>
2020-11-09 19:22:18 -0800

    Assignment 6 Code

---
+++ KipTatum/Assignment6/CompareString/CompareString/Program.cs
@@ -0,0 +1,56 @@
+﻿/********
+**
+**Name: Tatum, Kip
+**Class: CS 132
+**Project: 06 
+**Date: 2020-09-11
+**Description: program to compare 2 strings and display a message letting the user know if
+** the strings are equal or not
+*********/
+using System;
+
+namespace CompareString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool stringCompareResult; //declare a bool variable to get result of StringCompare()
+			bool stringCompareResult2; //declare a bool variable to get result of StringCompare()
+			
+			stringCompareResult = StringCompare("Hello", "hello");
+			//depending results of StringCompare() display proper message
+			if (stringCompareResult)
+			{
+				Console.Write("MATCH.\n");
+			}
+			else
+			{
+				Console.Write("DO NOT MATCH.\n");
+			}
+
+			stringCompareResult2 = StringCompare("Goodbye", "There");
+			//depending results of StringCompare() display proper message
+			if (stringCompareResult2)
+			{
+				Console.Write("MATCH.\n");
+			}
+			else
+			{
+				Console.Write("DO NOT MATCH.\n");
+			}
+
+			Console.ReadKey();
+		}
+
+		//This method will compare two strings regardless of casing and return true if
+		//they are the same and false if they are not
+		public static bool StringCompare(string one, string two)
+		{
+			string lowerOne = one.ToLower();
+			string lowerTwo = two.ToLower();
+			Console.Write($"{one} and {two} ");
+			return lowerOne.Equals(lowerTwo);
+		}
+	}
+}
---
+++ KipTatum/Assignment6/NumOut/NumOut/Program.cs
@@ -0,0 +1,32 @@
+﻿/********
+**
+**Name: Tatum, Kip
+**Class: CS 132
+**Project: 06 
+**Date: 2020-09-11
+**Description: program to manipulate and display a double in different ways
+*********/
+using System;
+
+namespace NumOut
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//declare the double
+			double givenVariable = 987654.321d;
+			string sGivenVariable = givenVariable.ToString(); //create the string representation of the double to manipulate it
+
+			Console.WriteLine($"{givenVariable} with an extra \".\" in it");
+			Console.WriteLine(sGivenVariable.Insert(3, ".")); //insert the "." after 7
+
+			Console.WriteLine($"{givenVariable} displayed as an integer");
+			Console.WriteLine(Convert.ToInt32(givenVariable)); //convert double into a variable
+
+			Console.WriteLine($"{givenVariable} displayed as US currency");
+			Console.WriteLine(givenVariable.ToString("C")); //pass "C" to ToString() to show as currency 
+			Console.ReadKey();
+		}
+	}
+}
