c3c12798c97f96b6398f254ce09fce2934f30a01
lukehc <lukehc@amazon.com>
2020-11-14 14:00:46 -0800

    PE06 Luke Chandler

---
+++ LukeChandler/ExpressionModified/ExpressionModified/Program.cs
@@ -0,0 +1,40 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS132
+**Project: Random PE06 Exercise 4
+**Date: 11-14-2020
+**Description: A program that modifies Expression project from Chapter 3
+*********/
+
+using System;
+
+namespace ExpressionModified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring 2 variables of type int with values of 100 and 20 respectively
+            int a;
+            int b;
+
+            //User Input
+            Console.WriteLine("Enter an integer value!");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second integer value!");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //Addition
+            Console.WriteLine("Addition: {0} + {1} = {2}", a, b, a + b);
+            //Subtraction
+            Console.WriteLine("Subtraction: {0} - {1} = {2}", a, b, a - b);
+            //Multiplication
+            Console.WriteLine("Multiplication: {0} * {1} = {2}", a, b, a * b);
+            //Division
+            Console.WriteLine("Division: {0} / {1} = {2}", a, b, a / b);
+            //Remainder
+            Console.WriteLine("Remainder: {0} % {1} = {2}", a, b, a % b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LukeChandler/NumOut/NumOut/Program.cs
@@ -0,0 +1,35 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS132
+**Project: Random PE06 Exercise 1
+**Date: 11-14-2020
+**Description: A program to play with the ToString method with a double variable
+*********/
+
+using System;
+
+namespace NumOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable of type double
+            double doubleNumber = 987654.321d;
+            Console.WriteLine("Original Double Variable = 987654.321");
+            //Double variable ToString
+            string doubleNumberToString = doubleNumber.ToString();
+            //Print out the doubleNumber variable with 2 decimal points
+            Console.WriteLine("Original Double Variable with 2 decimal points");
+            Console.WriteLine(doubleNumberToString.Insert(8, "."));
+            //Print out the doubleNumber variable in integer form
+            Console.WriteLine("Original Double Variable as an integer");
+            Console.WriteLine(Convert.ToUInt32(doubleNumber));
+            //Print out the doubleNumber variable as a currency value
+            Console.WriteLine("Original Double Variable as a currency value");
+            //https://www.tutorialspoint.com/chash-currency-c-format-specifier#:~:text=The%20%22C%22%20(or%20currency,C3%E2%80%9D)%20currency%20format%20specifier.
+            Console.WriteLine(doubleNumber.ToString("C"));
+            Console.ReadKey;
+        }
+    }
+}
