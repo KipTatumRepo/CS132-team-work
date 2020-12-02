0ff87d4daddb143ee5a6b2051ad6399a1f7da947
Kip Tatum <tatumkip@cityuniversity.edu>
2020-12-02 15:17:12 -0800

    Adding assignment 9

---
+++ KipTatum/Assignment9/Banking/Banking/Program.cs
@@ -0,0 +1,109 @@
+﻿/********
+**
+**Name: Tatum, Kip
+**Class: CS 132
+**Project: 09 
+**Date: 2020-30-11
+**Description:  This program will show various account balances after constructor are called with
+** global variables
+*********/
+using System;
+
+namespace Banking
+{
+	class Program
+	{
+		//base class 
+		class Account 
+		{
+			protected decimal BankBalance { get; set; }
+			protected Account()
+			{
+				Console.WriteLine();
+				Console.WriteLine("Inside the Account constructor");
+			}
+
+			//display the balance of the account
+			public void ShowBalance()
+			{
+				Console.WriteLine("The balance is " + BankBalance.ToString("C"));
+			}
+		}
+
+		//Savings class derived from account, this class can show a balance with interest applied
+		class Savings : Account
+		{
+			public Savings(decimal balance)
+			{
+				Console.WriteLine("Inside the default Savings constructor.");
+				BankBalance = balance;
+			}
+
+			//add monthly interest to the given balance of the savings account
+			public void AddMonthlyInterest(decimal interest)
+			{
+				BankBalance *= (1.0m + interest);
+			}
+		}
+
+		//Checking class derived from account, this class can show a balance after a checking account fee is deducted
+		class Checking : Account
+		{
+			public Checking(decimal balance)
+			{
+				Console.WriteLine("Inside the Checking constructor.");
+				BankBalance = balance;
+			}
+
+			//Take out the monthly service charge from the account balance
+			public void DeductServiceCharge()
+			{
+				const decimal CHECKING_FEE = 1.5M;
+				BankBalance -= CHECKING_FEE;
+			}
+		}
+
+		//A co-owned savings account that can show both owners and balance after interest is applied
+		class JointSavings : Savings
+		{
+			protected string firstNameA;
+			protected string firstNameB;
+
+			//Constructor that will take in the given names to assign the account to them
+			public JointSavings(string firstNameA, string firstNameB, decimal balance) : base(balance)
+			{
+				Console.WriteLine("Inside the JointSavings constructor.");
+				this.firstNameA = firstNameA;
+				this.firstNameB = firstNameB;
+			}
+
+			//display owners of the joint account
+			public void ShowOwners()
+			{
+				Console.WriteLine($"The owners of this joint account are {firstNameA} and {firstNameB}");
+			}
+		}
+		static void Main(string[] args)
+		{
+			const decimal OPENING_BALANCE = 55m;
+			const decimal MONTHLY_INTEREST = 0.0033m;
+
+			//add monthly interest to savings account and display balance
+			Savings savings = new Savings(OPENING_BALANCE);
+			savings.AddMonthlyInterest(MONTHLY_INTEREST);
+			savings.ShowBalance();
+
+			//deduct the monthly check fee from the account balance and display balance
+			Checking checking = new Checking(OPENING_BALANCE);
+			checking.DeductServiceCharge();
+			checking.ShowBalance();
+
+			//show the 2 owners of the joint account, plus add interest to the balance and display the balance
+			JointSavings jointSavings = new JointSavings("Bob", "Martha", OPENING_BALANCE);
+			jointSavings.ShowOwners();
+			jointSavings.AddMonthlyInterest(MONTHLY_INTEREST);
+			jointSavings.ShowBalance();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ KipTatum/Assignment9/Starter/Starter/Program.cs
@@ -0,0 +1,87 @@
+﻿/********
+**
+**Name: Tatum, Kip
+**Class: CS 132
+**Project: 09 
+**Date: 2020-11-30
+**Description: This program will display the fall sale of the fashion departments start and 
+** end date
+*********/
+using System;
+
+namespace Starter
+{
+	class Program
+	{
+		//a promotion has a name, start and end date
+		class Promotion
+		{
+			public string Name { get; private set; }
+			public DateTime Start { get; private set; }
+			public DateTime End { get; private set; }
+
+			//The constructor forces the entry of start and end dates, along with a promotion name
+			public Promotion(DateTime start, DateTime end, string name)
+			{
+				Start = start;
+				End = end;
+				Name = name;
+			}
+		}
+
+
+		//all classes will use this base class, each class will have different implentations
+		//of the members so it is abstract
+		abstract class Department
+		{
+			public abstract string DepartmentName { get; set; } //all departments need a name
+			protected Promotion[] sales; //there can be multiple sales in a department
+
+			//all classes need to assign promotions in some form
+			protected abstract void AssignPromotions();
+
+			//this is the preferred way to diplay promotions, however it can be customized per class
+			//if necessary
+			public virtual void DisplayPromotions()
+			{
+				Console.WriteLine($"{DepartmentName} Department Promotions: ");
+				foreach (Promotion sale in sales)
+				{
+					Console.WriteLine("Name:        {0}", sale.Name);
+					Console.WriteLine("Start Date:  {0}", sale.Start.ToString("m"));
+					Console.WriteLine("End Date:    {0}", sale.End.ToString("m"));
+				}
+			}
+		}
+
+		class Fashion : Department
+		{
+			public override string DepartmentName { get; set; }
+
+			//This class will always have the name Fashion so we assign it in the constructor
+			public Fashion()
+			{
+				DepartmentName = "Fashion";
+				AssignPromotions();
+			}
+
+			//the fashion class only needs to show start and end dates and the name of the promotion
+			protected override void AssignPromotions()
+			{
+				DateTime start = new DateTime(2020, 9, 1);
+				DateTime end = new DateTime(2020, 9, 15);
+				Promotion promo = new Promotion(start, end, "Fall Sale");
+
+				sales = new Promotion[] { promo };
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Fashion fashionDepartment = new Fashion();
+			fashionDepartment.DisplayPromotions();
+			Console.ReadKey();
+		}
+	}	
+}
+
