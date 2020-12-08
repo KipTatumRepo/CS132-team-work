0da97b8534574bc8fb476bd4c4201f0fa2fd3c65
lukehc <lukehc@amazon.com>
2020-12-08 15:49:06 -0800

    PE10

---
+++ LukeChandler/ColorList/ColorList/Program.cs
@@ -0,0 +1,78 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS132
+**Project: PE10E02 
+**Date: 2020-12-08
+**Description: Program that prints all of the colors in a list. Then subsitutes a color and checks if another is in the list
+*********/
+
+using System;
+using System.Collections.Generic;
+
+namespace ColorList
+{
+    class Program
+    {
+        //Method for printing values of the color list
+        private static void Print(List<string> list)
+        {
+            foreach (string color in list)
+            {
+                Console.WriteLine(color);
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Adding colors to the list
+            List<string> Colors = new List<string>();
+            Colors.Add("White");
+            Colors.Add("Red");
+            Colors.Add("Orange");
+            Colors.Add("Yellow");
+            Colors.Add("Green");
+            Colors.Add("Blue");
+            Colors.Add("Indigo");
+            Colors.Add("Violet");
+            Colors.Add("Black");
+
+            Print(Colors);
+
+            //Size of the List
+            int sizeOfList = Colors.Count;
+            Console.WriteLine("The list of colors includes {0} colors", sizeOfList);
+
+            //Sorting the List
+            Colors.Sort();
+            Console.WriteLine("\nThe colors sorted are");
+            Print(Colors);
+
+            //Getting index of and removing Violet and adding purple to penultimate index
+            int violetIndex = Colors.IndexOf("Violet");
+            Colors.RemoveAt(violetIndex);
+            //Add Purple at the penultimate index
+            Colors.Insert(violetIndex + 1, "Purple");
+            Print(Colors);
+
+            //check if there is Magenta
+            if (Colors.Contains("Magenta"))
+            {
+                Console.WriteLine("Magenta is in the list");
+            }
+
+            //Add Magenta and check again
+            Colors.Add("Magenta");
+            if (Colors.Contains("Magenta"))
+            {
+                Console.WriteLine("Magenta is in the list\n");
+            }
+
+            Print(Colors);
+
+
+            //Magenta was added to the end of the list. Insert is needed if we have a specific index in mind that we want to add it to.
+        }
+    }
+}
---
+++ LukeChandler/Swap/Swap/Program.cs
@@ -0,0 +1,49 @@
+﻿/********
+**Name: Chandler, Luke
+**Class: CS132
+**Project: PE10E01 
+**Date: 2020-12-08
+**Description: Program that takes in two values of different types and then swaps them
+*********/
+
+using System;
+
+namespace Swap
+{
+    class Program
+    {
+			static void Swap<T>(ref T a, ref T b)
+			{
+				//Displaying the values without swapping them
+				Console.WriteLine("Value of the first value is {0}", a);
+				Console.WriteLine("Value of the second value is {0}", b);
+
+				//Swap the values of a and b
+				T temp = a;
+				a = b;
+				b = temp;
+
+				//Displaying the values after swapping them
+				Console.WriteLine("Now, the value of the first value is {0}", a);
+				Console.WriteLine("Now, the value of the second value is {0}", b);
+			}
+
+			static void Main(string[] args)
+			{
+				int firstValueInt = 1986;
+				int secondValueInt = 2020;
+				string firstValueString = "Birth Year";
+				string secondValueString = "Current Year";
+
+				Swap<int>(ref firstValueInt, ref secondValueInt);
+
+				Console.WriteLine();
+
+				Swap<string>(ref firstValueString, ref secondValueString);
+
+				Console.ReadLine();
+
+			}
+		
+    }
+}
